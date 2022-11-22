757cb7334c8ba47a62e6fdd1de88026d1fbfbb89
Thuan Doan <thuandoan.arena@gmail.com>
2022-11-22 14:06:55 +0700

    edit files

--- main.js
+++ main.js
@@ -71,38 +71,6 @@ $(document).ready(function ($) {
     }
   });
 });
-//Count Up
-
-// const animationDuration = 5000;
-
-// const frameDuration = 1000 / 60;
-
-// const totalFrames = Math.round(animationDuration / frameDuration);
-
-// const easeOutQuad = (t) => t * (2 - t);
-
-// const animateCountUp = (el) => {
-//   let frame = 0;
-//   const countTo = parseInt(el.innerHTML, 10);
-
-//   const counter = setInterval(() => {
-//     frame++;
-
-//     const progress = easeOutQuad(frame / totalFrames);
-
-//     const currentCount = Math.round(countTo * progress);
-
-//     if (parseInt(el.innerHTML, 10) !== currentCount) {
-//       el.innerHTML = currentCount;
-//     }
-
-//     if (frame === totalFrames) {
-//       clearInterval(counter);
-//     }
-//   }, frameDuration);
-// };
-// const countupEls = document.querySelectorAll(".timer");
-// countupEls.forEach(animateCountUp);
 
 //back to top
 const toTop = document.querySelector(".to-top");
